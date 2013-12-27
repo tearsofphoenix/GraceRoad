f66f364348fc53a19feb3434e85362ff7f82ec02
Mac003 <Mac003@zhuzhiweis-iMac.local>
2013-12-27 15:42:24 +0800

    Initial Commit

---
+++ GraceRoad/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  GraceRoad
+//
+//  Created by Mac003 on 13-12-27.
+//  Copyright (c) 2013年 Mac003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GraceRoad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GraceRoad
+//
+//  Created by Mac003 on 13-12-27.
+//  Copyright (c) 2013年 Mac003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
